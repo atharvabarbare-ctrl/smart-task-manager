594e962c7dc50b78bfa7164f62211bc8aeba56bd
[atharvabarbare-ctrl] <[atharvabarbare@gmail.com]>
2026-06-30 17:10:21 +0530

    Updated API URL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://smart-task-manager-api-n9jf.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
